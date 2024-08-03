686492d1514ae0d179798f47fd6b3679dc836245
jibril04Afaa <128831088+jibril04Afaa@users.noreply.github.com>
2024-08-03 20:38:07 +0000

    added functionality

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,13 +1,23 @@
-import { useState, createElement } from "react"
+import { useState } from "react"
 
 const TodoList = () => {
-    const [taskAdded, settaskAdded] = useState();
+    const [ tasks, setTasks ] = useState([])
+    const [ newTask, setNewTask ] = useState("") // state for input value
+
     const addTask = () => {
-        // create new element with task name and task delete button
-        const newTask = createElement('div')
+        // check if new input is not empty
+        if (newTask.trim() !== "") {
+            // add new task to array
+            setTasks([...tasks, newTask]) 
+
+            // clear input field
+            setNewTask("")
+        }
     }
+
   return (
     <>
+
         <div className="border-solid border-2 border-purple-400 m-2 bg-purple-950">
             <div className="flex flex-col justify-around mx-4 my-4">
                 <div className="font-bold text-center text-white">
@@ -16,9 +26,29 @@ const TodoList = () => {
             </div>
 
             <div className="flex justify-around p-2">
-                <input type="text" placeholder="What is the task today?" className="border-solid border-2 p-2 border-purple-700 w-10/12" required />
+                <input type="text" 
+                placeholder="What is the task today?" 
+                className="border-solid border-2 p-2 
+                border-purple-700 w-10/12" 
+                required
+                value={newTask} // set input value to new task
+                onChange={(e) => setNewTask(e.target.value)} // update input state
+                 />
                 <button onClick={addTask} className="bg-purple-600 p-2">Add Task</button>
             </div>
+
+            {/* New task */}
+            <div className="p4">
+                <ul className="border-solid border-2 border-purple-400 m-2 bg-purple-600">
+                    {tasks.map((task, index) => (
+                        <div className="border-solid border-2 m-2">
+                            <li key={index} className="text-white p-4">{task}</li>
+                            <button className="text-white p-2">X</button>
+                        </div>   
+                    ))}
+                </ul>
+            </div>
+
         </div>
 
     </>
